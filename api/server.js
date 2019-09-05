4f974324a14e7c9eea581d7331281d99cf67e378
JasonYoo1 <jyoojs@gmail.com>
2019-09-05 10:57:07 -0700

    added commits

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 
+console.log("environment", process.env.NODE_ENV);
 const apiRouter = require('./api-router.js');
 
 const server = express();
